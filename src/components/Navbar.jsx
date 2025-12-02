22117d9e275b74c1a323b8569692c6caeb6bee2b
chandu <chandugit68@gmail.com>
2025-12-03 00:25:17 +0530

    navbar is developed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import mainImage from '../../public/mainImage.png'
+import {Link} from 'react-router-dom'
+import {CircleArrowOutUpRight } from 'lucide-react'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=' flex bg-gray-50 flex-row justify-between items-center p-7 -mt-3 '>
+      <Link to='/'>
+        <img 
+        className='h-5'
+        src={mainImage} alt="codedale" />
+      </Link>
+      <div className='flex flex-row gap-3 text-sm px-2 font-medium bg-white shadow-md  rounded-4xl py-2'>
+        <p className='hover:bg-gray-100 cursor-pointer rounded-4xl py-2 px-2'>Our Work</p>
+        <p className='hover:bg-gray-100 cursor-pointer rounded-4xl py-2 px-2'>Achievements</p>
+        <p className='hover:bg-gray-100 cursor-pointer rounded-4xl py-2 px-2'>FAQs</p>
+        <p className='hover:bg-gray-100 cursor-pointer rounded-4xl py-2 px-2'>Contact</p>
+      </div>
+      <div className='flex flex-row justify-center items-center gap-1 bg-black text-white p-3 rounded-4xl px-2 text-sm'>
+        <button className=' cursor-pointer'>Book a Call</button>
+        <CircleArrowOutUpRight size={16} color='white'  />
+      </div>
+    </div>
   )
 }
 
