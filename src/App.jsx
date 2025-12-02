4bef3de5451463baa9bae51571e28597979626e2
chandu <chandugit68@gmail.com>
2025-12-02 23:58:05 +0530

    layout developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import MainLayout from './mainlayout/MainLayout'
+import Home from './Home'
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+   <Router>
+    <Routes>
+      <Route path='/*' element={<MainLayout/>} >
+      <Route index element={<Home/>} />
+      </Route>
+    </Routes>
+   </Router>
   )
 }
 
